6794604ff7fe23b9bf0245c1eff11ac5d67b0c6f
Haseeb Ahmad <ahmad.has03@gmail.com>
2021-12-11 12:21:12 -0500

    first commit

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"port":        fmt.Sprintf("%d", app.config.port),
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
