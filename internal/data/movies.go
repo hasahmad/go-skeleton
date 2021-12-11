a721604d9006c249e461211eae94e0eb2f04b483
Haseeb Ahmad <ahmad.has03@gmail.com>
2021-12-11 14:46:03 -0500

    solve update race conditions issue with optimistic locking

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -45,7 +45,7 @@ func (m *MovieModel) Insert(movie *Movie) error {
 	query := `
 		INSERT INTO movies (title, year, runtime, genres)
 		VALUES ($1, $2, $3, $4)
-    	RETURNING id, created_at, version`
+		RETURNING id, created_at, version`
 
 	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
 
@@ -58,9 +58,9 @@ func (m *MovieModel) Get(id int64) (*Movie, error) {
 	}
 
 	query := `
-        SELECT id, created_at, title, year, runtime, genres, version
-        FROM movies
-        WHERE id = $1`
+		SELECT id, created_at, title, year, runtime, genres, version
+		FROM movies
+		WHERE id = $1`
 
 	var movie Movie
 	err := m.DB.QueryRow(query, id).Scan(
@@ -87,10 +87,10 @@ func (m *MovieModel) Get(id int64) (*Movie, error) {
 
 func (m *MovieModel) Update(movie *Movie) error {
 	query := `
-        UPDATE movies 
-        SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-        WHERE id = $5
-        RETURNING version`
+		UPDATE movies 
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5 AND version = $6
+		RETURNING version`
 
 	args := []interface{}{
 		movie.Title,
@@ -98,9 +98,20 @@ func (m *MovieModel) Update(movie *Movie) error {
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
 
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m *MovieModel) Delete(id int64) error {
@@ -109,8 +120,8 @@ func (m *MovieModel) Delete(id int64) error {
 	}
 
 	query := `
-        DELETE FROM movies
-        WHERE id = $1`
+		DELETE FROM movies
+		WHERE id = $1`
 
 	result, err := m.DB.Exec(query, id)
 	if err != nil {
