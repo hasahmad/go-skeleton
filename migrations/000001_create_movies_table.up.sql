5486220a7769090b6e53b313b5389b009b1f7683
Haseeb Ahmad <ahmad.has03@gmail.com>
2021-12-11 17:19:32 -0500

    updated migration

--- migrations/000001_create_movies_table.up.sql
+++ migrations/000001_create_movies_table.up.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS movies (
     id bigserial PRIMARY KEY,  
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    updated_at timestamp(0) with time zone,
+    deleted_at timestamp(0) with time zone,
     title text NOT NULL,
     year integer NOT NULL,
     runtime integer NOT NULL,
